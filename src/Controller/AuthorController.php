fedc03339a0222eaba7291e4b15e1681725740c9
Souleima18 <souleimab945@gmail.com>
2025-10-11 16:57:15 +0100

    formulaire d'ajout et de modification (aussi la fonction update)

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -5,9 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\AuthorRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Author;
 
 final class AuthorController extends AbstractController
@@ -21,47 +21,48 @@ final class AuthorController extends AbstractController
     }
 
     #[Route('/author/{name}', name: 'show_author')]
-public function showAuthor(string $name): Response
-{
-    return $this->render('author/show.html.twig', [
-        'name' => $name,
-    ]);
-}
-
-#[Route('/authors', name: 'list_authors')]
-public function listAuthors(): Response
-{
-    $authors = [
-        ['id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
-        ['id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
-        ['id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
-    ];
-
-    return $this->render('author/list.html.twig', [
-        'authors' => $authors,
-    ]);
-}
-
-#[Route('/author/details/{id}', name: 'author_details')]
-public function authorDetails(int $id): Response
-{
-    $authors = [
-        1 => ['id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
-        2 => ['id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
-        3 => ['id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
-    ];
-
-    $author = $authors[$id] ?? null;
-
-    if (!$author) {
-        throw $this->createNotFoundException("Auteur introuvable !");
+    public function showAuthor(string $name): Response
+    {
+        return $this->render('author/show.html.twig', [
+            'name' => $name,
+        ]);
     }
 
-    return $this->render('author/showAuthor.html.twig', [
-        'author' => $author,
-    ]);
-}
-#[Route('/getAll', name: 'app_get')]
+    #[Route('/authors', name: 'list_authors')]
+    public function listAuthors(): Response
+    {
+        $authors = [
+            ['id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
+            ['id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
+            ['id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
+        ];
+
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors,
+        ]);
+    }
+
+    #[Route('/author/details/{id}', name: 'author_details')]
+    public function authorDetails(int $id): Response
+    {
+        $authors = [
+            1 => ['id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
+            2 => ['id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
+            3 => ['id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
+        ];
+
+        $author = $authors[$id] ?? null;
+
+        if (!$author) {
+            throw $this->createNotFoundException("Auteur introuvable !");
+        }
+
+        return $this->render('author/showAuthor.html.twig', [
+            'author' => $author,
+        ]);
+    }
+
+    #[Route('/getAll', name: 'app_get')]
     public function getAllAuthor(AuthorRepository $authRepo): Response
     {
         $authors = $authRepo->findAll();
@@ -70,37 +71,85 @@ public function authorDetails(int $id): Response
             'authors' => $authors
         ]);
     }
+
     #[Route('/addAuth', name: 'app_add')]
     public function AddAuthor(ManagerRegistry $em): Response
     {
-        $auth1 =new Author();
+        $auth1 = new Author();
         $auth1->setUsername('Author1');
         $auth1->setEmail('author1@esprit.tn');
         
-        $auth2 =new Author();
+        $auth2 = new Author();
         $auth2->setUsername('Author2');
         $auth2->setEmail('author2@esprit.tn');
         
         $em->getManager()->persist($auth1);
         $em->getManager()->persist($auth2);
         $em->getManager()->flush();
-        return new Response('Author Added');
-    
-    }
-   #[Route('/author/delete/{id}', name: 'author_delete')]
-public function deleteAuthor(int $id, ManagerRegistry $doctrine, AuthorRepository $authRepo): Response
-{
-    $author = $authRepo->find($id);
-
-    if (!$author) {
-        throw $this->createNotFoundException("Auteur introuvable !");
+        return new Response('Authors Added');
     }
 
-    $em = $doctrine->getManager();
-    $em->remove($author);
-    $em->flush();
+    #[Route('/author/delete/{id}', name: 'author_delete')]
+    public function deleteAuthor(int $id, ManagerRegistry $doctrine, AuthorRepository $authRepo): Response
+    {
+        $author = $authRepo->find($id);
 
-    return $this->redirectToRoute('app_get'); // Redirige vers la liste après suppression
-}
+        if (!$author) {
+            throw $this->createNotFoundException("Auteur introuvable !");
+        }
 
+        $em = $doctrine->getManager();
+        $em->remove($author);
+        $em->flush();
+
+        return $this->redirectToRoute('app_get');
+    }
+
+    #[Route('/author/update/{id}', name: 'author_update')]
+    public function updateAuthor(int $id, Request $request, ManagerRegistry $doctrine, AuthorRepository $authRepo): Response
+    {
+        $author = $authRepo->find($id);
+
+        if (!$author) {
+            throw $this->createNotFoundException("Auteur introuvable !");
+        }
+
+        if ($request->isMethod('POST')) {
+            $username = $request->request->get('username');
+            $email = $request->request->get('email');
+
+            $author->setUsername($username);
+            $author->setEmail($email);
+
+            $em = $doctrine->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('app_get');
+        }
+
+        return $this->render('author/update.html.twig', [
+            'author' => $author,
+        ]);
+    }
+
+    #[Route('/authadd', name: 'author_add_form')]
+    public function addAuthorForm(Request $request, ManagerRegistry $doctrine): Response
+    {
+        if ($request->isMethod('POST')) {
+            $username = $request->request->get('username');
+            $email = $request->request->get('email');
+
+            $author = new Author();
+            $author->setUsername($username);
+            $author->setEmail($email);
+
+            $em = $doctrine->getManager();
+            $em->persist($author);
+            $em->flush();
+
+            return $this->redirectToRoute('app_get');
+        }
+
+        return $this->render('author/add.html.twig');
+    }
 }
